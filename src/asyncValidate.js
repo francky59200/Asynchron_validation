8208e9d541145626746ed3000a6d83b9c77d87a1
francky59200 <49784881+francky59200@users.noreply.github.com>
2020-01-25 15:40:27 +0100

    Initial commit

---
+++ src/asyncValidate.js
@@ -0,0 +1,15 @@
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const asyncValidate = (values /*, dispatch */) => {
+  return sleep(1000).then(() => {
+    // simulate server latency
+    if (["john", "paul", "george", "ringo"].includes(values.username)) {
+      throw { username: "That username is taken" };
+    }
+    if (["jhonny", "eric"].includes(values.password)) {
+      throw { password: "password is faild" };
+    }
+  });
+};
+
+export default asyncValidate;
